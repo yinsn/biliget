5ac1acffa976b75838ab29676d365df4d657c5b3
yin.cheng <yin.cheng@smartnews.com>
2022-08-28 12:46:53 +0800

    download check

---
+++ src/download/__init__.py
@@ -0,0 +1,5 @@
+from .download import DownLoader
+
+__all__ = [
+    "DownLoader",
+]
---
+++ src/download/download.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+
+class DownLoader:
+    def __init__(self, save_path: Optional[str]) -> None:
+        self.save_path = save_path
+        self._check_path()
+
+    def _check_path(self) -> None:
+        """Check if the existence of path for dumped data.
+        Create one if not.
+        """
+        if not self.save_path:
+            self.save_path = os.path.join(os.getcwd(), "bilivideos")
+        else:
+            self.save_path = os.path.join(os.getcwd(), self.save_path)
+        if not Path(self.save_path).exists():
+            os.makedirs(self.save_path)
